2b21659c35490104f877eac1f940f0b8f925f852
Pooja-CodeHub <sonawanepooja047@gmail.com>
2026-01-17 22:36:47 +0530

    Resolve auth layout import and routing --Uday.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 import { Routes, Route, Navigate } from "react-router-dom";
+
+import AuthLayout from "./layouts/authLayout";
 import DashboardLayout from "./layouts/dashboardLayout";
+
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 import Overview from "./components/Overview";
 import Users from "./components/Users";
@@ -9,6 +14,13 @@ import Settings from "./components/Settings";
 function App() {
   return (
     <Routes>
+      {/* Auth routes */}
+      <Route element={<AuthLayout />}>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Route>
+
+      {/* Dashboard routes */}
       <Route path="/" element={<DashboardLayout />}>
         <Route index element={<Navigate to="overview" />} />
         <Route path="overview" element={<Overview />} />
